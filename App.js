dada34fb90f3289c0649ec4e711b721dac9f7c23
Nsomuah82 <jasomuah002@st.ug.edu.gh>
2024-07-01 19:52:53 +0000

    changed font size to 24

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Janet Adu Somuah</Text>
+      <Text style={styles.text}>My name is Janet Adu Somuah</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,9 @@ const styles = StyleSheet.create({
     backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text:{
+    color: 'white',
+    fontSize: 24,
   },
 });
