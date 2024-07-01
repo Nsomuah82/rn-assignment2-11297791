d5efded5bc05cc9fe74ec5bf433dd82de97c79ab
Nsomuah82 <jasomuah002@st.ug.edu.gh>
2024-07-01 19:59:17 +0000

    Applied boldstyle to my name

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Janet Adu Somuah</Text>
+     <Text style={styles.text}>My name is <Text style={styles.boldText}>Janet Adu Somuah</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,5 +20,8 @@ const styles = StyleSheet.create({
   text:{
     color: 'white',
     fontSize: 24,
+  },
+  boldText:{
+    fontWeight: 'bold',
   },
 });
